7e2a1487f577b79bc44d22e1b2de0d4f6ecb950e
Xkonti <xkonti@xkonti.tech>
2024-05-07 00:55:57 -0400

    Implement "writeImpl"

--- src/osFs.nim
+++ src/osFs.nim
@@ -54,3 +54,12 @@ method readAllImpl(self: OsFs, absolutePath: Path): string =
     return readFile(absolutePath.string)
   except IOError as e:
     raise newException(FileSystemError, e.msg, e)
+
+method writeImpl(self: OsFs, absolutePath: Path, content: varargs[string, `$`]) =
+  var text = ""
+  for value in content:
+    text.add value 
+  try:
+    writeFile(absolutePath.string, text)
+  except IOError as e:
+    raise newException(FileSystemError, e.msg, e)
